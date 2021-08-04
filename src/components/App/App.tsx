a2bb8e720b37e4a8107835a0f3fa35d9d10e76dd
Azir <thoromuerte@gmail.com>
2021-08-04 17:45:57 +0300

    feat: add html for todo

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+import { AddIcon } from "../icons/AddIcon";
 
 export function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          FUCK!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="todo-app">
+        <div className="app-menu">
+          <div className="menu-top-lane">
+            <span className="date-day">
+              <strong>Wednesday,</strong> 4
+            </span>
+            <span className="counter">X Tasks</span>
+          </div>
+          <div className="menu-bottom-lane">
+            <span className="date-month">August</span>
+            <button className="button" type="button">
+              CLEAR LIST
+            </button>
+          </div>
+          <div className="menu-filters">
+            <button className="button">All</button>
+            <button className="button">Active</button>
+            <button className="button">Completed</button>
+          </div>
+        </div>
+        <div className="todo-input-container">
+          <span className="todo-input-icon">
+            <AddIcon />
+          </span>
+          <input
+            className="todo-input"
+            type="text"
+            placeholder="Type your task"
+          />
+        </div>
+        <div className="todo-list-item">
+          <input className="checkbox" type="checkbox" />
+          <span className="item-text">Do some shit</span>
+          <span className="item-date">4:20</span>
+          <button className="button" type="button">
+            DELETE
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
